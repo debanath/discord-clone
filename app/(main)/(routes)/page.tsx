150ed956f2af10bc214c67fc1cbd93e80c3d8970
Debashish Nath <57856461+debanath@users.noreply.github.com>
2023-12-14 01:03:28 +0530

    added authentication

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <p className="text-3xl font-bold text-indigo-500">Hello world</p>
+      <Button>Click me</Button>
+    </div>
+  );
+}
