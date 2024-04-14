be3c529eb6c7fb71a4679115e270653e58c30265
Debashish Nath <57856461+debanath@users.noreply.github.com>
2024-04-15 02:12:04 +0530

    add navtion items and also trying to swap db

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -3,6 +3,8 @@ import { currentProfile } from "@/lib/current-profile";
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 
+import { NavigationAction } from "./navigation-action";
+
 export const NavigationSidebar = async () => {
     const profile = await currentProfile();
 
@@ -23,7 +25,7 @@ export const NavigationSidebar = async () => {
     return (
         <>
             <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
-                Hello
+                <NavigationAction />
             </div>
         </>
     );
