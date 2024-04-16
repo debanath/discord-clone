4c191560739b55e212757aefa3a48fd16dafea72
Debashish Nath <57856461+debanath@users.noreply.github.com>
2024-04-16 19:30:51 +0530

    add createServer Modal

--- components/modals/create-server-modal.tsx
+++ components/modals/create-server-modal.tsx
@@ -60,6 +60,7 @@ export const CreateServerModal = () => {
 
             form.reset();
             router.refresh();
+            onClose();
         } catch (error) {
             console.log(error);
         }
