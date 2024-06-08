0259f0402f3ff0eabd3bb525b87db2839daba157
Debashish Nath <57856461+debanath@users.noreply.github.com>
2024-06-09 03:07:01 +0530

    feat: Add channel and conversations pages

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,8 +1,50 @@
-const ServerIdPage = () => {
-    return (
-        <div>
-            <p>Server Id page</p>
-        </div>
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ServerIdPageProps {
+    params: {
+        serverId: string;
+    };
+}
+
+const ServerIdPage = async ({ params }: ServerIdPageProps) => {
+    const profile = await currentProfile();
+
+    const server = await db.server.findUnique({
+        where: {
+            id: params.serverId,
+            members: {
+                some: {
+                    profileId: profile?.id,
+                },
+            },
+        },
+        include: {
+            channels: {
+                where: {
+                    name: "general",
+                },
+                orderBy: {
+                    createdAt: "asc",
+                },
+            },
+        },
+    });
+
+    const initialChannel = server?.channels[0];
+
+    if (initialChannel?.name !== "general") {
+        return null;
+    }
+
+    if (!profile) {
+        return redirectToSignIn();
+    }
+    return redirect(
+        `/servers/${params.serverId}/channels/${initialChannel?.id}`
     );
 };
 
