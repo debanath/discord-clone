a165df118cb35bbce1b87d65c340ad41d846892a
Debashish Nath <57856461+debanath@users.noreply.github.com>
2024-06-10 11:00:37 +0530

    feat: Complete Socket.IO setup

--- types.ts
+++ types.ts
@@ -1,4 +1,16 @@
+import { Server as NetServer, Socket } from "net";
+import { NextApiResponse } from "next";
+import { Server as SocketIOServer } from "socket.io";
 import { Server, Profile, Member } from "@prisma/client";
+
 export type ServerWithMembersWithProfiles = Server & {
     members: (Member & { profile: Profile })[];
+};
+
+export type NextApiServerResponseIo = NextApiResponse & {
+    socket: Socket & {
+        server: NetServer & {
+            io: SocketIOServer;
+        };
+    };
 };
