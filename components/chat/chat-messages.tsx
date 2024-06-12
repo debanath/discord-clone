4cf9824cb50bbf9d1f636219548ad16235f7b499
Debashish Nath <57856461+debanath@users.noreply.github.com>
2024-06-12 19:34:30 +0530

    feat: add chat edit and delete functionality

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import { Fragment } from "react";
+import { format } from "date-fns";
 import { Member, Message, Profile } from "@prisma/client";
 import { Loader2, ServerCrash } from "lucide-react";
 
 import { useChatQuery } from "@/hooks/use-chat-query";
 
 import { ChatWelcome } from "./chat-welcome";
+import { ChatItem } from "./chat-item";
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
 type MessageWithMemberWithProfile = Message & {
     member: Member & {
@@ -78,7 +82,24 @@ export const ChatMessages = ({
                     <Fragment key={i}>
                         {group.item.map(
                             (message: MessageWithMemberWithProfile) => (
-                                <div key={message.id}>{message.content}</div>
+                                <ChatItem
+                                    key={message.id}
+                                    id={message.id}
+                                    currentMember={member}
+                                    member={message.member}
+                                    content={message.content}
+                                    fileUrl={message.fileUrl}
+                                    deleted={message.deleted}
+                                    timestamp={format(
+                                        new Date(message.createdAt),
+                                        DATE_FORMAT
+                                    )}
+                                    isUpdated={
+                                        message.updatedAt !== message.createdAt
+                                    }
+                                    socketUrl={socketUrl}
+                                    socketQuery={socketQuery}
+                                />
                             )
                         )}
                     </Fragment>
